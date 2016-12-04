cb61da5592654c3d7b3689797bd8eda8abaaed5b
Karthik <karthik@Karthiks-MacBook-Pro-2.local>
2016-12-04 14:31:41 -0500

    seperated code to both historical.json and stream.json files

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ let request = require('request'),
 let config = require('./config');
 jsonfile.spaces = 4;
 
-let runStream = () => {
-    console.log('starting stream');
+let runHistorical = () => {
+    console.log('starting historical data');
     let url = config.api_host;
     let headers = {
         'Content-Type': 'application/json', 
         Authorization: 'Token token="'+config.api_token+'"'
     }
     let options = {url, headers}
-    fetchData(options);
+    fetchHistoricalData(options);
 }
 
-let fetchData = (options) => {
+let fetchHistoricalData = (options) => {
     console.log('fetch data');
     //https://github.com/request/request#custom-http-headers
     request(options, function (error, response, body) {
@@ -28,13 +28,13 @@ let fetchData = (options) => {
                 let last_id = last_result.id - 100;
                 console.log(last_id, results.length)
                 let next_options = _.merge(options, {url: config.api_host + '?last_id=' + last_id});
-                var file = './data.json';
+                var file = './history.json';
                 //https://github.com/jprichardson/node-jsonfile#writefilefilename-obj-options-callback
                 
                 //IMP: Either append and update single json file OR OR OR bulk upload to ES
                 jsonfile.writeFile(file, results, {flag: 'a'}, function (json_err) {
                     if (json_err) console.error(json_err);
-                    fetchData(next_options);
+                    fetchHistoricalData(next_options);
                 });
                 
             } catch(err) {
@@ -46,4 +46,59 @@ let fetchData = (options) => {
     });
 }
 
+let runStream = () => {
+    console.log('starting stream');
+    let url = config.api_host;
+    let headers = {
+        'Content-Type': 'application/json', 
+        Authorization: 'Token token="'+config.api_token+'"'
+    }
+    let options = {url, headers}
+    fetchStreamData(options);
+}
+
+let fetchStreamData = (options) => {
+    console.log('fetch data');
+    //https://github.com/request/request#custom-http-headers
+    request(options, function (error, response, body) {
+        if (!error && body) {
+            try {
+                let results = JSON.parse(body);
+                let most_recent_result = results[0]; //first 
+                if (most_recent_result) {
+                    let last_id = most_recent_result.id;
+                    let next_options = _.merge(options, {url: config.api_host + '?last_id=' + last_id});
+                } else {
+                    //no results
+                    let next_options = options;
+                }
+                
+                console.log(last_id, results.length);
+                var time_period = 10000;
+                //https://github.com/jprichardson/node-jsonfile#writefilefilename-obj-options-callback
+                
+                //TODO: check empty results response and see if this code keeps repeating
+                if (results.length > 0) {
+                    var file = './stream.json';
+                    jsonfile.writeFile(file, results, {flag: 'a'}, function (json_err) {
+                        if (json_err) console.error(json_err);
+                        setTimeout(fetchStreamData(next_options), time_period);
+                    });
+                } else {
+                    setTimeout(fetchStreamData(next_options), time_period);
+                }
+
+                
+            } catch(err) {
+                console.log("We’ve encountered parsing json error?: " + err); //couldnt parse, body not proper json
+            }
+        } else {
+            console.log("We’ve encountered an error: " + error);
+        }
+    });
+}
+
+//Storing in json files allow us to avoid express app. We just periodically pick up from stream.json file.
+// Storing in ES lets us periodically query for recent most data. Requires express app, etc. 
+// runHistorical();
 runStream();
